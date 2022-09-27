ea2d6c76bc580dae5d7b9fdb3479a5183a365a05
Beni Young <55400759+beni-young@users.noreply.github.com>
2022-09-26 23:11:03 -0500

    added setDecrease and setIncrease functions

---
+++ Components/CreationCounter.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import { View, Text, Stylesheet, Button } from 'react-native';
+
+const CreationCounter = (props) => {   
+    
+
+    return <View>
+        <Button title="Decrease" onPress={props.onDecrease}/>
+        <Text>Current {props.name}: {props.value}</Text>
+        <Button title="Increase" onPress={props.onIncrease} />
+    </View>
+}
+
+
+
+export default CreationCounter;
--- src/screens/Creation.js
+++ src/screens/Creation.js
@@ -1,38 +1,79 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import CreationCounter from '../../Components/CreationCounter';
 
 
 const Creation = () => {
 
-    const [counter, setCounter] = useState(0);
-
-    const [strength, setStrength] = useState(0);
+    const [strength, setStrength] = useState(1);
     const [health, setHealth] = useState(10);
     const [magic, setMagic] = useState(1);
-    const [total, setTotal] = useState(15);
+    const [total, setTotal] = useState(15); 
+
+    const setDecrease = (type) => {
+
+        if(type === 'strength'){
+            strength - 1 < 1 ? null : (setStrength(strength - 1), setTotal(total + 1));
+        }
+
+        if(type === 'health'){
+            health - 10 < 10 ? null : (setHealth(health - 10), setTotal(total + 1));
+        }
+
+        if(type === 'magic'){
+            magic - 1 < 1 ? null : (setMagic(magic - 1), setTotal(total + 1));
+        }
+    }
+
+    const setIncrease = (type) => {
+
+        if(type === 'strength'){
+            total - 1 < 0 ? null : (setStrength(strength + 1), setTotal(total - 1)); 
+        }
+
+        if(type === 'health'){
+            total - 1 < 0  ? null : (setHealth(health + 10), setTotal(total - 1)); 
+        }
+
+        if(type === 'magic'){
+            total - 1 < 0  ? null : (setMagic(magic + 1), setTotal(total - 1));
+        }
+    }
 
 
-    console.log(counter);
+    //console.log(total);
     return <View>
       <Text>Grow in Power</Text>
-      <Button title="Decrease" onPress={function(){ setStrength(strength - 1); setTotal(total + 1); console.log(strength);}} />
-      <Text>Current strength: {strength} </Text>
-      <Button title="Increase" onPress={function(){ setStrength(strength + 1); setTotal(total - 1); console.log(strength);}} />
-
-      <Button title="Decrease" onPress={function(){ setHealth(health - 10); setTotal(total + 1); console.log(health);}} />
-      <Text>Current health: {health}</Text>
-      <Button title="Increase" onPress={function(){ setHealth(health + 10); setTotal(total - 1); console.log(health);}} />
-
-      <Button title="Decrease" onPress={function(){ setMagic(magic - 1); setTotal(total + 1); console.log(magic);}} />
-      <Text>Current magic: {magic}</Text>
-      <Button title="Increase" onPress={function(){ setMagic(magic + 1); setTotal(total - 1); console.log(magic);}} />
-
+      <Text></Text>
+      <Text></Text>
+      <CreationCounter name="strength" value={strength}
+      onDecrease={ () => {setDecrease('strength')}}       
+      onIncrease={ () => {setIncrease('strength')}}    
+      />
+      <Text></Text>
+      <Text></Text>
+      <CreationCounter name="health" value={health}
+      onDecrease={ () => {setDecrease('health')}}       
+      onIncrease={ () => {setIncrease('health')}}  
+      />
+      <Text></Text>
+      <Text></Text>
+      <CreationCounter name="magic" value={magic}
+      onDecrease={ () => {setDecrease('magic')}}       
+      onIncrease={ () => {setIncrease('magic')}}      
+      />
+      <Text></Text>
+      <Text></Text>
       <Text>Points Remaining: {total}</Text>
-
+      <Text></Text>
+      <Text></Text>
       <Text>Spend all your points to continue!</Text>
-      <Button title="Click Me To Embark On Your Journey!" onPress={function(){ counter; console.log(counter);}} />
-    </View>
-    /* return <Text style={styles.text}>Fight for Glory!</Text>; */
+      <Text></Text>
+      <Text></Text>
+      <Button title="Click Me To Embark On Your Journey!" />
+   </View>
+    
+     
   }
 
 

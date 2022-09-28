aeb6706f06c3ed4ec11ee1ca0118d5cc84c96c51
Beni Young <55400759+beni-young@users.noreply.github.com>
2022-09-27 21:55:23 -0500

    added button change to different screens

--- src/screens/Creation.js
+++ src/screens/Creation.js
@@ -2,13 +2,28 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 import CreationCounter from '../../Components/CreationCounter';
 
+const STAT_SCREEN_STATE = "characterCreation";
+const COMBAT_SCREEN_STATE = "dancing";
 
-const Creation = () => {
+
+const Creation = () => {   
+
+    const [gameState, setGameState] = useState(STAT_SCREEN_STATE);
+    var whatToDisplay;
 
     const [strength, setStrength] = useState(1);
     const [health, setHealth] = useState(10);
     const [magic, setMagic] = useState(1);
     const [total, setTotal] = useState(15); 
+
+    loginHandler = (state) => {
+
+      if(state ==='d') {
+        return setGameState(COMBAT_SCREEN_STATE);
+      } else {
+        return setGameState(STAT_SCREEN_STATE);
+      }
+    }
 
     const setDecrease = (type) => {
 
@@ -40,44 +55,48 @@ const Creation = () => {
         }
     }
 
+    switch(gameState){    
+      case STAT_SCREEN_STATE:
+        whatToDisplay = <View>
+          <Text>Grow in Power</Text>
+          <Text></Text>
+          <Text></Text>
+          <CreationCounter name="strength" value={strength}
+          onDecrease={ () => {setDecrease('strength')}}       
+          onIncrease={ () => {setIncrease('strength')}}    
+          />
+          <Text></Text>
+          <Text></Text>
+          <CreationCounter name="health" value={health}
+          onDecrease={ () => {setDecrease('health')}}       
+          onIncrease={ () => {setIncrease('health')}}  
+          />
+          <Text></Text>
+          <Text></Text>
+          <CreationCounter name="magic" value={magic}
+          onDecrease={ () => {setDecrease('magic')}}       
+          onIncrease={ () => {setIncrease('magic')}}      
+          />
+          <Text></Text>
+          <Text></Text>
+          <Text>Points Remaining: {total}</Text>
+          <Text></Text>
+          <Text></Text> 
+          { total > 0 ? <Text>Spend all your points to continue!</Text>: null }
+          { total === 0 ? <Button title="Click Me To Embark On Your Journey!"
+            onPress={() => loginHandler('d')}/> : null}
+          </View>
+        break;
+    case COMBAT_SCREEN_STATE:
+        whatToDisplay = <View><Text>Dancing!</Text>
+        <Button title="Go Back!" onPress={() => loginHandler('c')}/> 
+        
+        </View>
+        break;
 
-    //console.log(total);
-    return <View>
-      <Text>Grow in Power</Text>
-      <Text></Text>
-      <Text></Text>
-      <CreationCounter name="strength" value={strength}
-      onDecrease={ () => {setDecrease('strength')}}       
-      onIncrease={ () => {setIncrease('strength')}}    
-      />
-      <Text></Text>
-      <Text></Text>
-      <CreationCounter name="health" value={health}
-      onDecrease={ () => {setDecrease('health')}}       
-      onIncrease={ () => {setIncrease('health')}}  
-      />
-      <Text></Text>
-      <Text></Text>
-      <CreationCounter name="magic" value={magic}
-      onDecrease={ () => {setDecrease('magic')}}       
-      onIncrease={ () => {setIncrease('magic')}}      
-      />
-      <Text></Text>
-      <Text></Text>
-      <Text>Points Remaining: {total}</Text>
-      <Text></Text>
-      <Text></Text>
-      <Text>Spend all your points to continue!</Text>
-      <Text></Text>
-      <Text></Text>
-      <Button title="Click Me To Embark On Your Journey!" />
-   </View>
-    
-     
+    }
+    return whatToDisplay;
   }
-
-
-
 
 const styles = StyleSheet.create({
     text: {
